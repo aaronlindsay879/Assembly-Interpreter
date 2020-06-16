0b1545a067564e1ca6f0bab0f0cadca740463a1f
Aaron <aarlin5409@hessleacademy.com>
2020-06-16 20:22:47 +0100

    Updated UI to allow user to input commands

--- Assembly Interpreter/Program.cs
+++ Assembly Interpreter/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,25 +15,22 @@ namespace Assembly_Interpreter
         private DataStorage memory;
         private DataStorage registers;
         private Program program;
+        private Thread runThread;
 
         public MyForm()
         {
             memory = new DataStorage(100, true);
             registers = new DataStorage(8, true);
-            program = new Program(new Command("LDR R0,#2"), new Command("LDR R1,#5"), new Command("ADD R2,R0,R1"), new Command("STR R2,0"));
-            Debug.WriteLine(registers);
-
             InitComponents();
 
-            Task.Run(() => program.Execute(ref memory, ref registers));
-
-            Timer timer = new Timer();
-            timer.Tick += new EventHandler(OnTimedEvent);
+            //Create timer to update screen every 0.1 seconds
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Tick += new EventHandler(UpdateScreen);
             timer.Interval = 100;
             timer.Enabled = true;
         }
 
-        public void OnTimedEvent(object source, EventArgs e)
+        public void UpdateScreen(object source, EventArgs e)
         {
             Controls["mem"].Text = "Memory:\n" + memory.CreateOutput();
             Controls["reg"].Text = "Registers:\n" + registers.CreateOutput(1);
@@ -40,25 +38,45 @@ namespace Assembly_Interpreter
 
         void InitComponents()
         {
+            //Create all UI elements
             TextBox textBox = new TextBox
             {
                 Size = new Size(240, 500),
                 Location = new Point(5, 5),
-                Multiline = true
+                Multiline = true,
+                Name = "Code"
             };
 
             Button button = new Button
             {
-                Size = new Size(100, 40),
+                Size = new Size(80, 40),
                 Location = new Point(5, 505),
-                Text = "Run code"
+                Text = "Run code",
+                Name = "RunCode"
             };
 
             Button buttonTwo = new Button
             {
-                Size = new Size(100, 40),
-                Location = new Point(105, 505),
-                Text = "Stop code"
+                Size = new Size(80, 40),
+                Location = new Point(85, 505),
+                Text = "Stop code",
+                Name = "StopCode"
+            };
+
+            Label delayLabel = new Label
+            {
+                Size = new Size(80, 20),
+                Location = new Point(165, 505),
+                Text = "Delay (s)"
+            };
+
+            NumericUpDown delay = new NumericUpDown
+            {
+                Size = new Size(80, 20),
+                Location = new Point(165, 525),
+                Increment = 0.1M,
+                DecimalPlaces = 1,
+                Name = "Delay"
             };
 
             Label memory = new Label
@@ -79,12 +97,40 @@ namespace Assembly_Interpreter
                 Name = "reg"
             };
 
+            //Add event listeners to run code and stop code buttons
+            button.Click += new EventHandler(RunCode_Click);
+            buttonTwo.Click += new EventHandler(StopCode_Click);
+
+            //Add all elements to screen
             Controls.Add(textBox);
             Controls.Add(button);
             Controls.Add(buttonTwo);
+            Controls.Add(delayLabel);
+            Controls.Add(delay);
             Controls.Add(memory);
             Controls.Add(registers);
             Size = new System.Drawing.Size(1000, 600);
+        }
+
+        public void RunCode_Click(object sender, EventArgs e)
+        {
+            //Get the text from the textbox, and split on every newline
+            string textBox = Controls["Code"].Text;
+            string[] splitText = textBox.Split(new[] { "\r\n" }, StringSplitOptions.None);
+
+            //Convert all the strings into commands
+            Command[] commands = splitText.Select(x => new Command(x)).ToArray();
+
+            float.TryParse(Controls["Delay"].Text, out float delay);
+            program = new Program(commands);
+            
+            //Create a thread which will execute commands one by one
+            runThread = new Thread(() => program.Execute(ref memory, ref registers, delay));
+            runThread.Start();
+        }
+        public void StopCode_Click(object sender, EventArgs e)
+        {
+            runThread.Abort();
         }
 
         [STAThread]
